b8c7c6b6c5e2a6aa21ee1261a5ffcd72fd7ae378
Дмитрий Зырянов <zyryanovd@macbook-pro-dmitrij.local>
2018-10-18 22:15:17 +0300

    Initial Commit

---
+++ CoffeeHubex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeHubex
+//
+//  Created by Дмитрий Зырянов on 18.10.2018.
+//  Copyright © 2018 FutureMind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
